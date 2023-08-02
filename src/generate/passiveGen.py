433eb39c01b4349ce1fa8dc825aa0176fe1b54e6
Im-a-degen <robbyrafky@gmail.com>
2023-08-02 05:57:07 +0100

    Add skeleton for passive generation

---
+++ src/generate/passiveGen.py
@@ -0,0 +1,20 @@
+import json
+
+with open("data/passiveData.json") as f:
+    data = json.load(f)
+
+
+def create_passive(passive_type, position):
+    pass
+
+
+def add_passive_name_description(name, description):
+    pass
+
+
+def add_passive_stats(stats, math, values, function,):
+    pass
+
+
+def add_passive_requirements(required, connections):
+    pass
---
+++ src/test.py
@@ -0,0 +1,8 @@
+
+def dance_with_death():
+    pass
+
+
+passive_functions = {
+    "dance_with_death": dance_with_death
+}
