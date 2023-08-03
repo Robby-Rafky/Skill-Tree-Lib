0c5eb54e5a2afd7b90b0d2cefe418d10f6167dc3
Im-a-degen <robbyrafky@gmail.com>
2023-08-03 04:38:51 +0100

    Rearrange Files

---
+++ src/visual/skill_tree/skillTree.py
@@ -0,0 +1,8 @@
+from src.visual.coloursImports import *
+
+# TODO
+# ADD "CURRENTLY SELECTED" (NAME/ID) TO THE BOTTOM OF THE SKILL TREE UI
+
+class SkillTreeSurface:
+    def __init__(self):
+        pass
