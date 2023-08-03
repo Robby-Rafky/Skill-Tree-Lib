e98be7a50164e09a6a78b039b47b8aa80189fdba
Im-a-degen <robbyrafky@gmail.com>
2023-08-03 04:38:38 +0100

    Rearrange Files Pull Classes From Gardening Game Trim Classes To Be More Efficient

---
+++ src/visual/classes/button.py
@@ -0,0 +1,74 @@
+from src.visual.classes.textBox import *
+
+
+class Button(TextBox):
+    """Represents a button in the game.
+
+    The Button class extends the TextBox class and provides functionality for creating interactive buttons.
+
+    Attributes:
+        rect (pygame.Rect): The rectangular area enclosing the button.
+
+    Methods:
+        __init__(text, position, size, offset, outline, centered, font_size, colour=GREY):
+            Initializes a new instance of the Button class.
+        update_button(text, colour):
+            Updates the text and colour of the button.
+        update_button_position(text, colour, x, y):
+            Updates the position, text, and colour of the button.
+        button_event(mouse_coordinates):
+            Checks if a button event has occurred.
+
+    """
+
+    def __init__(self, text, position, size, colour=(235, 169, 94)):
+        """
+        Initializes a new instance of the Button class.
+
+        Args:
+            text (str): The text displayed on the button.
+            position (tuple): The position of the button (x, y).
+            size (tuple): The size of the button (width, height).
+            colour (tuple, optional): The colour of the button. Defaults to GREY.
+        """
+        TextBox.__init__(self, text, position, size, centered=True, colour=colour)
+        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+
+    def update_button(self, text, colour):
+        """
+        Updates the text and colour of the button.
+
+        Args:
+            text (str): The new text for the button.
+            colour (tuple): The new colour for the button.
+        """
+        self.update_textbox(text, colour)
+        self.rect = pygame.Rect(self.x, self.y, self.size[0], self.size[1])
+
+    def update_button_position(self, text, colour, x, y):
+        """
+        Updates the position, text, and colour of the button.
+
+        Args:
+            text (str): The new text for the button.
+            colour (tuple): The new colour for the button.
+            x (int): The new x-coordinate of the button.
+            y (int): The new y-coordinate of the button.
+        """
+        self.x, self.y = x, y
+        self.update_button(text, colour)
+
+    def button_event(self, mouse_coordinates):
+        """
+        Checks if a button event has occurred.
+
+        Args:
+            mouse_coordinates (tuple): The coordinates of the mouse (x, y).
+
+        Returns:
+            bool: True if a button event has occurred, False otherwise.
+        """
+        if pygame.mouse.get_pressed()[0]:
+            if self.rect.collidepoint(mouse_coordinates):
+                return True
+            return False
---
+++ src/visual/classes/textBox.py
@@ -0,0 +1,94 @@
+from src.visual.coloursImports import *
+
+
+class TextBox:
+    """Represents a text box.
+
+    Attributes:
+        text_visual (pygame.Surface): The rendered text surface.
+        x (int): The x-coordinate of the text box.
+        y (int): The y-coordinate of the text box.
+        size (tuple): The size of the text box.
+        font (pygame.font.Font): The font used for the text.
+        centered (bool): Indicates whether the text is centered within the text box.
+        text (str): The text content of the text box.
+        surface (pygame.Surface): The surface of the text box.
+        text_colour (tuple): The colour of the text.
+
+    Methods:
+        update_textbox(text, colour=WHITE):
+            Updates the content and colour of the text box.
+        update_textbox_multiline(text, colour=WHITE):
+            Updates the multiline content and colour of the text box.
+        draw_on_surface(surface):
+            Draws the text box on a given surface.
+    """
+    def __init__(self, text, position, size, centered, colour=GREY):
+        """
+        Initializes a new instance of the TextBox class.
+
+        Args:
+            text (str): The text content of the text box.
+            position (tuple): The position of the text box (x, y).
+            size (tuple): The size of the text box (width, height).
+            outline (bool): Indicates whether the text box has an outline.
+            centered (bool): Indicates whether the text is centered within the text box.
+            font_size (int): The font size of the text.
+            colour (tuple, optional): The colour of the text box. Defaults to GREY.
+        """
+        self.text_visual = None
+        self.x, self.y = position
+        self.size = size
+        self.font = pygame.font.SysFont("arial", 20)
+        self.text = text
+        self.centered = centered
+        self.surface = pygame.Surface(self.size)
+        self.text_colour = BLACK
+        self.update_textbox(text, colour)
+
+    def update_textbox(self, text, colour=WHITE):
+        """
+        Updates the content and colour of the text box.
+
+        Args:
+            text (str): The text content of the text box.
+            colour (tuple, optional): The colour of the text box. Defaults to WHITE.
+        """
+        self.surface.fill(colour)
+        self.text_visual = self.font.render(text, True, self.text_colour)
+        if self.centered:
+            self.surface.blit(self.text_visual, (self.size[0]/2 - self.text_visual.get_width()/2,
+                                                 self.size[1]/2 - self.text_visual.get_height()/2))
+        else:
+            self.surface.blit(self.text_visual, (10, 10))
+
+        pygame.draw.rect(self.surface, BLACK, (0, 0, self.size[0], self.size[1]), 2)
+
+    def update_textbox_multiline(self, text, colour=WHITE):
+        """
+        Updates the multiline content and colour of the text box.
+
+        Args:
+            text (str): The multiline text content of the text box.
+            colour (tuple, optional): The colour of the text box. Defaults to WHITE.
+        """
+        self.surface.fill(colour)
+        self.text = text
+        for x in range(len(text)):
+            self.text_visual = self.font.render(text[x], True, self.text_colour)
+            if self.centered:
+                self.surface.blit(self.text_visual, (self.size[0] / 2 - self.text_visual.get_width() / 2,
+                                                     self.size[1] / 2 - self.text_visual.get_height() / 2
+                                                     + self.text_visual.get_height() * x))
+            else:
+                self.surface.blit(self.text_visual, (10, 10 + self.text_visual.get_height() * x))
+        pygame.draw.rect(self.surface, BLACK, (0, 0, self.size[0], self.size[1]), 2)
+
+    def draw_on_surface(self, surface):
+        """
+        Draws the text box on a given surface.
+
+        Args:
+            surface (pygame.Surface): The surface to draw the text box on.
+        """
+        surface.blit(self.surface, (self.x, self.y))
