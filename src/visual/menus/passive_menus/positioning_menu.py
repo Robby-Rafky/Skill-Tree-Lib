4d2ef3867e1943ecb9e2c4cea2d832d9cc54258b
Im-a-degen <robbyrafky@gmail.com>
2023-08-05 06:28:11 +0100

    Add menu manager reference for switching menu types and accessing other object variables Add button layout and skeleton for the positioning menu

--- src/visual/menus/passive_menus/positioning_menu.py
+++ src/visual/menus/passive_menus/positioning_menu.py
@@ -1,17 +1,39 @@
+from src.visual.colours_imports import *
 from src.visual.classes.button import Button
+from src.visual.classes.text_box import TextBox
 #required
 
 class PositionMenu:
     def __init__(self, menu_manager_ref):
-        self.button_create_passive = Button("beans", (40, 40), (400, 140))
-        self.button_save_skill_tree = Button("beans", (1280, 40), (400, 60))
-        self.button_load_skill_tree = Button("beans", (1180, 120), (400, 60))
+        self.menu_manage_ref = menu_manager_ref
+        self.positioning_info = TextBox(" ", (40, 40), (600, 70), False)
+        self.passive_position = TextBox("Current Passive Location: (_, _) ", (40, 130), (600, 50), False)
+        self.button_move_up = Button("↑", (940, 20), (80, 80))
+        self.button_move_down = Button("↓", (940, 120), (80, 80))
+        self.button_move_left = Button("←", (840, 70), (80, 80))
+        self.button_move_right = Button("→", (1040, 70), (80, 80))
+        self.button_submit_position = Button("Lock In Position", (1480, 40), (400, 140))
+
+        self.positioning_info.update_textbox_multiline(["Select the location of the passive,",
+                                                       "use the arrows to move the passive by 1 unit."], L_GREY)
 
     def menu_events(self, mouse_position):
-        if self.button_create_passive.button_event(mouse_position):
-            print("ye")
+        if self.button_move_up.button_event(mouse_position):
+            print("up")
+        if self.button_move_down.button_event(mouse_position):
+            print("down")
+        if self.button_move_left.button_event(mouse_position):
+            print("left")
+        if self.button_move_right.button_event(mouse_position):
+            print("right")
+        if self.button_submit_position.button_event(mouse_position):
+            self.menu_manage_ref.current_menu = "Type"
 
     def draw_menu(self, surface):
-        self.button_create_passive.draw_on_surface(surface)
-        self.button_save_skill_tree.draw_on_surface(surface)
-        self.button_load_skill_tree.draw_on_surface(surface)
+        self.positioning_info.draw_on_surface(surface)
+        self.passive_position.draw_on_surface(surface)
+        self.button_move_up.draw_on_surface(surface)
+        self.button_move_down.draw_on_surface(surface)
+        self.button_move_left.draw_on_surface(surface)
+        self.button_move_right.draw_on_surface(surface)
+        self.button_submit_position.draw_on_surface(surface)
